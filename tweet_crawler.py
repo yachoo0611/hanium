52feaca61430914965a8ef65f6e6dc599e0046fa
cwgwak <yhntfx777@naver.com>
2020-08-02 15:50:05 +0900

    json1

--- tweet_crawler.py
+++ tweet_crawler.py
@@ -14,12 +14,33 @@ for tweet in list_of_tweets:
 from twitterscraper.query import query_tweets 
 import csv 
 import datetime 
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, RelationsOptions
 
-keyword = '코로나바이러스' 
+keyword = 'covid' 
 f = open(keyword+'.csv','w',encoding='utf-8-sig',newline='') 
 w = csv.writer(f,delimiter=',') 
-list_of_tweets = query_tweets(keyword, begindate=datetime.date(2020,7,1), enddate=datetime.date(2020,7,2), limit=5) 
+list_of_tweets = query_tweets(keyword, begindate=datetime.date(2020,7,27), enddate=datetime.date(2020,8,1), limit=5) 
 
 for tweet in list_of_tweets: 
     w.writerow([tweet.timestamp, tweet.text]) 
 f.close()
+
+
+authenticator = IAMAuthenticator('Fuxoqi_ltW0gcE6PZkYT-lMS8zsY0Xtd7AfaKzqesa_W')
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2019-07-12',
+    authenticator=authenticator
+)
+
+natural_language_understanding.set_service_url('https://api.kr-seo.natural-language-understanding.watson.cloud.ibm.com/instances/f85b9cf9-3ab1-477c-8627-5dd173ced2c1')
+
+f = open('covid.csv','rt',encoding='utf-8-sig')
+rdr = csv.reader(f)
+with open("testfile2.json","w") as json_file:
+    for line in rdr:
+        response = natural_language_understanding.analyze(text=line[1],features=Features(relations=RelationsOptions()),language='en').get_result()
+
+        json.dump(response, json_file, indent=2)
