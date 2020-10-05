0a12be26aa1ccd47aa6e0340474bcdb5a297d114
cwgwak <yhntfx777@naver.com>
2020-10-05 14:10:13 +0900

    September assign

--- depandemic/models.py
+++ depandemic/models.py
@@ -1,14 +1,18 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+#from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.CharField(max_length=200,db_column='author')
-    title = models.CharField(max_length=200)
-    contents = models.TextField()
-    categorized_contents = models.TextField()
-    score = models.FloatField()
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.CharField(max_length=144,null=True)
+    identifier = models.IntegerField(null=False)
+    title = models.CharField(max_length=200,null=True)
+    location = models.TextField(default="Unknown")
+    contents = models.TextField(null=True)
+    categorized_contents = models.TextField(null=True)
+    score = models.IntegerField(null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
--- new_ver.py
+++ new_ver.py
@@ -14,28 +14,38 @@ import django
 django.setup()
 from depandemic.models import Post
 
+#API 키 등의 초기화는 최상단에 한번만 호출
+consumer_key = 'WH7O98QsTNHInE7Osn9I6bjom'
+consumer_secret = 'd16T80KdkQ7zB8TmAjcjOG2vtPryuA7i1R3KJ0oTfxt3PCxmJX'
+atoken = '1295969346800345088-xYSWmzhn0zHj6BLeISi1SlGa5ufQ9t'
+asecret = 'rdkj7OaQVK6xQKmscMVZGERJwjzzYRp53THTXOjiFI7F2'
+auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
+api = tweepy.API(auth)
+
+authenticator = IAMAuthenticator('Fuxoqi_ltW0gcE6PZkYT-lMS8zsY0Xtd7AfaKzqesa_W')
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2019-07-12',
+    authenticator=authenticator
+)
+
+natural_language_understanding.set_service_url(
+    'https://api.kr-seo.natural-language-understanding.watson.cloud.ibm.com/instances/f85b9cf9-3ab1-477c-8627-5dd173ced2c1')
 
 def twitter_crwaler():
-    consumer_key = 'WH7O98QsTNHInE7Osn9I6bjom'
-    consumer_secret = 'd16T80KdkQ7zB8TmAjcjOG2vtPryuA7i1R3KJ0oTfxt3PCxmJX'
-    atoken = '1295969346800345088-xYSWmzhn0zHj6BLeISi1SlGa5ufQ9t'
-    asecret = 'rdkj7OaQVK6xQKmscMVZGERJwjzzYRp53THTXOjiFI7F2'
-    auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
-    api = tweepy.API(auth)
-
-    authenticator = IAMAuthenticator('Fuxoqi_ltW0gcE6PZkYT-lMS8zsY0Xtd7AfaKzqesa_W')
-    natural_language_understanding = NaturalLanguageUnderstandingV1(
-        version='2019-07-12',
-        authenticator=authenticator
-    )
-
-    natural_language_understanding.set_service_url(
-        'https://api.kr-seo.natural-language-understanding.watson.cloud.ibm.com/instances/f85b9cf9-3ab1-477c-8627-5dd173ced2c1')
-
+    
     send_data = {}
     end_data2 = []
+    cursor = tweepy.Cursor(api.search, 
+                        q='코로나',
+                        since='2020-09-23', # 2020-09-08 이후에 작성된 트윗들로 가져옴
+                        count=2  # 페이지당 반환할 트위터 수 1
+                        ).items(50) #최대 100개 까지만
+    for i, tweet in enumerate(cursor):
+        #print("{}: {}".format(i, tweet.text))
+        send_data['location'] = "Unknown"
+        send_data['categorized'] = ""
+        send_data['score'] = ""
 
-    for tweet in tweepy.Cursor(api.search, q='코로나', since='2020-08-31', until='2020-09-07').items(10):
         response = natural_language_understanding.analyze(
             text=tweet.text,
             features=Features(
@@ -46,23 +56,31 @@ def twitter_crwaler():
             )
         ).get_result()
 
+        for re in response['entities']:
+            if re['type'] == "Location":
+                send_data['location'] = re['text']
+            else:
+                send_data['location'] = "Unknown"
+
+        for re1 in response['categories']:
+            send_data['categorized'] = re1['label']
+            send_data['score'] = re1['score']
+
         send_data['author'] = tweet.author.name
         send_data['title'] = tweet.author.id
         send_data['contents'] = tweet.text
         send_data['created'] = tweet.created_at
         send_data['published'] = datetime.datetime.now()
-        for re1 in response['categories']:
-            send_data['categorized'] = re1['label']
-            send_data['score'] = re1['score']
+
         dictionary_copy = send_data.copy()
         end_data2.append(dictionary_copy)
+
     return end_data2
 
 
 if __name__=='__main__':
     data1 = twitter_crwaler()
     for x in data1:
-        Post(author=x['author'],title=x['title'],contents=x['contents'],created_date=x['created'],
+        Post(author=x['author'],identifier=0,location=x['location'],title=x['title'],contents=x['contents'],created_date=x['created'],
              published_date=x['published'],categorized_contents=x['categorized'],score=x['score']).save()
-
 
